5879ab7315915fc2ee68abe555fdb0d94a8106c9
Neil Figueroa <solosfact@gmail.com>
2021-11-27 23:34:10 -0500

    Version 1.0.0 frontend Geekvolution con angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: "inicio",
+  component: InicioComponent
+  },
+  {
+    path:"seguridad",
+    loadChildren:()=> import("./modulos/seguridad/seguridad.module").then(x=>x.SeguridadModule)
+  },
+  {
+    path: "",
+    pathMatch:"full",
+    redirectTo:"/inicio"
+  },
+  {
+    path:"**",
+    component:ErrorComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
